a755d16c6453cc84307b1380dadd8fe37b759034
ntflix <felix@iron59.co.uk>
2024-07-01 20:21:04 +0100

    initial commit

---
+++ main/BridgingHeader.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
---
+++ main/Dummy.c
@@ -0,0 +1,1 @@
+// We need to have at least one .c file for the ESP-IDF CMake build system to work.
